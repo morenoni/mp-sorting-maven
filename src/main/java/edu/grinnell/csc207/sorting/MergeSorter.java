d69b271e285e9d27caeb68e0612a62b911b2d290
Nicole_Moreno <nickymoreno1110@gmail.com>
2024-11-11 15:56:37 -0600

    Merge implemented?

--- src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
@@ -55,6 +55,68 @@ public class MergeSorter<T> implements Sorter<T> {
    */
   @Override
   public void sort(T[] values) {
-    // STUB
+    if (values.length < 2) {
+      return; 
+    } // if
+    T[] temp = values.clone();
+    mergeSort(values, temp, 0, values.length - 1);
   } // sort(T[])
+
+  /**
+   * Recursively divides the array and sorts each part using merge sort.
+   *
+   * @param values
+   *   The array to be sorted.
+   * @param temp
+   *   A temporary array used for merging.
+   * @param start
+   *   The starting index of the portion to be sorted.
+   * @param end
+   *   The ending index of the portion to be sorted.
+   */
+  private void mergeSort(T[] values, T[] temp, int start, int end) {
+    if (start < end) {
+      int mid = start + (end - start) / 2;
+      mergeSort(values, temp, start, mid);
+      mergeSort(values, temp, mid + 1, end);
+      merge(values, temp, start, mid, end);
+    } // if
+  } // mergeSort
+
+  /**
+   * Merges two sorted halves of an array.
+   *
+   * @param values
+   *   The array containing the sorted halves.
+   * @param temp
+   *   A temporary array used to store the merged result.
+   * @param leftStart
+   *   The starting index of the first sorted half.
+   * @param mid
+   *   The ending index of the first sorted half (midpoint).
+   * @param rightEnd
+   *   The ending index of the second sorted half.
+   */
+  private void merge(T[] values, T[] temp, int leftStart, int mid, int rightEnd) {
+    int leftIndex = leftStart;      
+    int rightIndex = mid + 1;       
+    int tempIndex = leftStart;      
+    // Merge elements from both halves into temp
+    while (leftIndex <= mid && rightIndex <= rightEnd) {
+      if (order.compare(values[leftIndex], values[rightIndex]) <= 0) {
+        temp[tempIndex++] = values[leftIndex++];
+      } else {
+        temp[tempIndex++] = values[rightIndex++];
+      } // else 
+    } // while
+    while (leftIndex <= mid) {
+      temp[tempIndex++] = values[leftIndex++];
+    } // while
+    while (rightIndex <= rightEnd) {
+      temp[tempIndex++] = values[rightIndex++];
+    } // while
+    for (int i = leftStart; i <= rightEnd; i++) {
+      values[i] = temp[i];
+    } // for
+  } // merge
 } // class MergeSorter
